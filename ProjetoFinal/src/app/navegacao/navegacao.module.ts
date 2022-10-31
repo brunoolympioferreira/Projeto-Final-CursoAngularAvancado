464e3968deea91c01e3584ad7f1e20f88ef06260
Bruno Ferreira <bruno.olympio.ferreira@gmail.com>
2022-10-31 17:25:01 -0300

    commit inicial do projeto

---
+++ ProjetoFinal/src/app/navegacao/navegacao.module.ts
@@ -0,0 +1,29 @@
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  declarations: [
+    MenuComponent,
+    HomeComponent,
+    FooterComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    NgbModule
+  ],
+  exports: [
+    MenuComponent,
+    HomeComponent,
+    FooterComponent,
+    NotFoundComponent
+  ]
+})
+export class NavegacaoModule { }
